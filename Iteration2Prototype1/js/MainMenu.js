4134da86784cee719a9d6fbfe53492f4dd1b86d2
swilli60 <36717810+swilli60@users.noreply.github.com>
2018-04-29 14:57:50 -0400

    Add files via upload

--- Iteration2Prototype1/js/MainMenu.js
+++ Iteration2Prototype1/js/MainMenu.js
@@ -6,9 +6,12 @@ GameStates.makeMainMenu = function( game, shared ) {
 	//var playButton = null;
 	let titleText = null;
 	let keys = null;
-	let description = ["This game takes place on a colony living on a space station en route to their eventual new home planet that space command has charted out, though that new home is several years of travel away. Two players take on the role of two evil masterminds trying to take over the spaceship by means of infiltrating the government and being elected to leader. These masterminds are sworn nemeses and take any opportunity to best the other, or, given the opportunity, take them out of the picture, permanently.",
-	null];
+	let description = ["This game takes place on a colony living on a space station en route to their eventual new home planet that space command has charted out, though that new home is several years of travel away. Two players take on the role of two evil masterminds trying to take over the spaceship by means of infiltrating the government and being elected to leader. (1/4)",
+        "These masterminds are sworn nemeses and take any opportunity to best the other, or, given the opportunity, take them out of the picture, permanently.(2/4)",
+	    "There has been an emergency on-board the colony. During an attack, space pirates blasted a hole in the hull of the colony. Most colonists had evacuated to the inner part of the ship, but a few stragglers were in the section of ship exposed by the blast from the pirates. (3/4)",
+        "They are without space suits and must be rescued quickly, before they suffocate and die, but more importantly, before your nemesis does the same and takes credit for it! Become the \“hero\” of the colony, winning the hearts and minds of those who can elect you into power! (4/4)"];
 	let descriptionIndex = 0;
+	let textBox = null;
     
     
 
@@ -33,13 +36,13 @@ GameStates.makeMainMenu = function( game, shared ) {
             titleText = game.add.text(game.world.centerX, game.world.centerY - 180, "Nemeses!", style);
             titleText.anchor.setTo(0.5, 0.5);
 
-            let textBox = game.add.graphics(0, 0);
+            textBox = game.add.graphics(0, 0);
             textBox.beginFill(0x0000FF);
             textBox.drawRoundedRect(5, 225, 790, 235, 9);
             textBox.endFill();
 
             style = { font: "25px Verdana", fill: "#FFFFFF", align: "center", wordWrap: true, wordWrapWidth: game.world.width };
-            textBox.data = game.add.text(game.world.centerX, game.world.centerY + 25, description[0], style);
+            textBox.data = game.add.text(game.world.centerX, game.world.centerY + 35, description[0], style);
             textBox.data.anchor.setTo(0.5, 0.5);
 
             
@@ -47,27 +50,27 @@ GameStates.makeMainMenu = function( game, shared ) {
             let instructions = game.add.text(game.world.centerX, 585, "Use left and right arrows to navigate instructions.", style);
             instructions.anchor.setTo(0.5, 0.5);
 
-            //this.showButton(infoButton, infoButtonLocation, 'Information', null);
-
-            keys = game.input.keyboard.addKeys({'help': Phaser.KeyCode.H, 'info': Phaser.KeyCode.SPACEBAR, 'previous': Phaser.KeyCode.LEFT, 'next': Phaser.KeyCode.RIGHT});
-
-            //game.add.group();
-            //    //game.add.graphics(0, 0);
-            //textBox.beginFill(0x0000FF);
-            //textBox.drawRoundedRect(5, 225, 790, 235, 9);
-            //textBox.endFill();
+            keys = game.input.keyboard.addKeys({'help': Phaser.KeyCode.H, 'info': Phaser.KeyCode.SPACEBAR, 'prev': Phaser.KeyCode.LEFT, 'next': Phaser.KeyCode.RIGHT});
     
         },
     
         update: function () {
-            if (keys.next.isDown)
-            { }
-            //	Do some nice funky main menu effect here
+            if(keys.next.justDown)
+                this.nextText();
+            if (keys.prev.justDown)
+                this.prevText();
+            
     
         },
 
         nextText: function () {
-            let a = null;
+            if(descriptionIndex < description.length - 1)
+                textBox.data.text = description[++descriptionIndex];
+        },
+
+        prevText: function () {
+            if (descriptionIndex > 0)
+                textBox.data.text = description[--descriptionIndex];
         }
         
     };
