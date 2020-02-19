e182239bdf5c7da26e1541c11ab17d32ee1494a6
Scott Williams <swilli60@gmu.edu>
2020-02-19 12:22:41 -0500

    Modified main menu instructions slightly

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -27,7 +27,7 @@ BasicGame.MainMenu.prototype = {
 		this.textBox.endFill();
 
 		this.style = { font: "25px Verdana", fill: "#FFFFFF", align: "center", wordWrap: true, wordWrapWidth: this.game.world.width };
-		this.text = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 25, "Use the mouse to select two points that will form the basis for a line that will reveal part of the game board. The smaller and harder-to-find circles are worth more points! There are four circles, and you will hear a ding when a circle is found. The player with the most points once all circles have been found wins!", this.style);
+		this.text = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 25, "Use the mouse to select two points that will form the basis for a line that will reveal part of the game board. There are four solid white circles, and you will hear a ding when a circle is found. The smaller and harder-to-find circles are worth more points! The player with the most points once all circles have been found wins!", this.style);
 		this.text.anchor.setTo(0.5, 0.4);
 	},
 
